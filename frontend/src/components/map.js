6b3835852989945cd51e39fbdff870ae893743a2
Jono Mingard <reason.koan@gmail.com>
2018-01-07 15:54:54 +1300

    Add basic tile graphics

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -2,8 +2,9 @@ import m from "mithril"
 
 export default function Map(vnode) {
   function view() {
+    console.log(vnode.attrs.game)
     return m(".map", vnode.attrs.game.tiles.map(tile =>
-      m(".tile")
+      m(".tile", { class: tile.type_id, style: `--x: ${tile.x}; --y: ${tile.y}` })
     ))
   }
 

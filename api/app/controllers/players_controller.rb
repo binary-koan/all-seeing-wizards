771406243d3a1bde6be26935f691f6d08a696c06
Jono Mingard <reason.koan@gmail.com>
2018-02-04 13:48:15 +1300

    Remove unnecessary controller

--- api/app/controllers/players_controller.rb
+++
@@ -1,6 +0,0 @@
-class PlayersController < ApplicationController
-  def show
-    player = Player.find(params[:id])
-    render json: player.as_json(root: true, methods: [:connected], include: { game: { methods: [:started] }, character: {}, player_cards: { include: :card } })
-  end
-end

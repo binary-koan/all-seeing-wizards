9ab965ff8a526dd2f08acf5c8542b412d948b02f
Jono Mingard <reason.koan@gmail.com>
2018-04-07 01:34:45 +1200

    Scaffold a basic Cycle.js project with Parcel

---
+++ frontend-cycle/src/main.ts
@@ -0,0 +1,16 @@
+import { h1, makeDOMDriver } from "@cycle/dom"
+import { run } from "@cycle/run"
+import xs from "xstream"
+
+function main() {
+  const sinks = {
+    DOM: xs.periodic(1000).map(i => h1("" + i + " seconds elapsed"))
+  }
+  return sinks
+}
+
+const drivers = {
+  DOM: makeDOMDriver("#app")
+}
+
+run(main, drivers)

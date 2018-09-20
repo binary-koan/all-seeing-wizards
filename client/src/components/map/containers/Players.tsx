59e179c54199db101abdab941306f94b032363fc
Jono Mingard <reason.koan@gmail.com>
2018-09-20 21:33:41 +0900

    Show players on the map

--- client/src/components/map/containers/Players.tsx
+++ client/src/components/map/containers/Players.tsx
@@ -1,6 +1,39 @@
-import { Container } from "@inlet/react-pixi"
+import { Container, Sprite } from "@inlet/react-pixi"
 import React from "react"
+import { connect } from "react-redux"
+import ViewState from "../../../state/viewState"
+import { MapViewScaleProps, withMapViewScale } from "../MapViewScaleContext"
 
-const Players: React.SFC = _props => <Container />
+import data from "../../../../../packs/base/viewConfig"
 
-export default Players
+interface StateProps {
+  players: Array<{
+    x: number
+    y: number
+    image: string
+  }>
+}
+
+const Players: React.SFC<StateProps & MapViewScaleProps> = props => (
+  <Container>
+    {props.players.map(player => (
+      <Sprite
+        image={player.image}
+        {...props.mapViewScale.tilePosition(player)}
+        {...props.mapViewScale.tileSize}
+      />
+    ))}
+  </Container>
+)
+
+function mapStateToProps(state: ViewState): StateProps {
+  return {
+    players: state.game.players.toArray().map(player => ({
+      x: player.position.x,
+      y: player.position.y,
+      image: data.characters[player.character.name].image
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(withMapViewScale(Players))

1eafdf817699dca40081feafabf96aafebc86154
Jono Mingard <reason.koan@gmail.com>
2018-11-26 21:31:13 +0900

    Add effect images for water and lava

--- client/src/components/map/containers/Players.tsx
+++ client/src/components/map/containers/Players.tsx
@@ -1,11 +1,16 @@
 import { Container, Sprite } from "@inlet/react-pixi"
 import React from "react"
 import { connect } from "react-redux"
+import { DirectionalPoint } from "../../../../../common/src/state/directionalPoint"
+import { Game } from "../../../../../common/src/state/game"
 import ViewState from "../../../state/viewState"
-import { MapViewScaleProps, withMapViewScale } from "../MapViewScaleContext"
-
-import data from "../../../../packs/base/viewConfig"
 import tweener from "../../util/tweener"
+import { MapViewScaleProps, withMapViewScale } from "../MapViewScaleContext"
+import PointEffectImage from "../results/PointEffectImage"
+
+import lavaFireImage from "../../../../assets/effects/lava-fire.png"
+import waterSlowImage from "../../../../assets/effects/water-slow.png"
+import data from "../../../../packs/base/viewConfig"
 
 const TweenedSprite = tweener(Sprite, {
   x: { duration: 500 },
@@ -18,21 +23,37 @@ interface StateProps {
     x: number
     y: number
     image: string
+    environmentEffectImage?: string
   }>
 }
 
 const Players: React.SFC<StateProps & MapViewScaleProps> = props => (
   <Container>
     {props.players.map(player => (
-      <TweenedSprite
-        key={player.id}
-        image={player.image}
-        {...props.mapViewScale.mapPosition(player)}
-        {...props.mapViewScale.tileSize}
-      />
+      <Container>
+        <TweenedSprite
+          key={player.id}
+          image={player.image}
+          {...props.mapViewScale.mapPosition(player)}
+          {...props.mapViewScale.tileSize}
+        />
+        <PointEffectImage
+          imagePath={player.environmentEffectImage}
+          position={{ x: player.x, y: player.y }}
+          alpha={player.environmentEffectImage ? 1 : 0}
+        />
+      </Container>
     ))}
   </Container>
 )
+
+function effectImageForPosition(position: DirectionalPoint, game: Game) {
+  if (game.board.tileAt(position).type === "water") {
+    return waterSlowImage
+  } else if (game.board.tileAt(position).type === "lava") {
+    return lavaFireImage
+  }
+}
 
 function mapStateToProps(state: ViewState): StateProps {
   return {
@@ -40,7 +61,8 @@ function mapStateToProps(state: ViewState): StateProps {
       id: player.id,
       x: player.position.x,
       y: player.position.y,
-      image: data.characters[player.character.name].images[player.position.facing]
+      image: data.characters[player.character.name].images[player.position.facing],
+      environmentEffectImage: effectImageForPosition(player.position, state.game)
     }))
   }
 }
--- client/src/components/map/results/PointEffectImage.tsx
+++ client/src/components/map/results/PointEffectImage.tsx
@@ -5,11 +5,13 @@ import { MapViewScaleProps, withMapViewScale } from "../MapViewScaleContext"
 interface PointEffectImageProps {
   position: { x: number; y: number }
   imagePath: string
+  alpha?: number
 }
 
 const PointEffectImage: React.SFC<PointEffectImageProps & MapViewScaleProps> = props => (
   <Sprite
     image={props.imagePath}
+    alpha={props.alpha || 1}
     {...props.mapViewScale.tileSize}
     {...props.mapViewScale.mapPosition(props.position)}
   />

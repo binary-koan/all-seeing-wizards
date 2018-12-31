c687ced505026b6f68303c32ff78850666d2de05
Jono Mingard <reason.koan@gmail.com>
2019-01-01 12:21:09 +1300

    Tweak result display timing to make it slightly easier to understand

--- client/src/animation/timing.ts
+++ client/src/animation/timing.ts
@@ -1,6 +1,6 @@
 export const RESULTS_RECEIVED_DELAY = 500
 
-export const PRIORITY_DURATION = 800
+export const PRIORITY_DURATION = 700
 export const MOVE_DURATION = 500
-export const ATTACK_DURATION = 800
-export const BETWEEN_ACTIONS_DELAY = 200
+export const ATTACK_DURATION = 1100
+export const BETWEEN_ACTIONS_DELAY = 1100
